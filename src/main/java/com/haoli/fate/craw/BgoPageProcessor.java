52e7bd68baca182e1c4abe3f7c568537e761bb2d
lihao <lihao_100@boe.com.cn>
2019-01-25 18:10:54 +0800

    fix

--- src/main/java/com/haoli/fate/craw/BgoPageProcessor.java
+++ src/main/java/com/haoli/fate/craw/BgoPageProcessor.java
@@ -2,9 +2,13 @@ package com.haoli.fate.craw;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSONObject;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -20,29 +24,27 @@ public class BgoPageProcessor implements PageProcessor{
 
     private Site site = Site.me().setSleepTime(3000);
     
-	List<String> ids = new ArrayList<String>();
+    List<String> newsList = new ArrayList<String>();
+    
+    List<String> newsDetailList = new ArrayList<String>();
 	
-	List<String> titleList = new ArrayList<String>();
-	
-	List<String> contentList = new ArrayList<String>();
-
     @Override
     public void process(Page page) {
 		if (page.getUrl().regex(LIST_URL).match()) {
 			page.putField("title", new JsonPathSelector("$.data[*].title").selectList(page.getRawText()));
-			ids = new JsonPathSelector("$.data[*].id").selectList(page.getRawText());
-			titleList = new JsonPathSelector("$.data[*].title").selectList(page.getRawText());
-			page.putField("titleList", titleList);
-	        if (CollectionUtils.isNotEmpty(ids)) {
-	            for (String id : ids) {
+			List<String> idList = new JsonPathSelector("$.data[*].id").selectList(page.getRawText());
+			newsList = new JsonPathSelector("$.data[*]").selectList(page.getRawText());
+			page.putField("newsList", newsList);
+	        if (CollectionUtils.isNotEmpty(idList)) {
+	            for (String id : idList) {
 	                page.addTargetRequest("https://api.biligame.com/news/" + id + ".action");
 	            }
 	        }
 		}
 		else {
-	        String content = new JsonPathSelector("$.data.content").select(page.getRawText());
-	        contentList.add(content);
-	        page.putField("content", content);
+			String newsDetail = new JsonPathSelector("$.data[*]").select(page.getRawText());
+	        newsDetailList.add(newsDetail);
+	        page.putField("newsDetailList", newsDetailList);
 		}
 
     }
@@ -51,11 +53,56 @@ public class BgoPageProcessor implements PageProcessor{
     public Site getSite() {
         return site;
     }
+    
+	public List<JSONObject> getResult() {
+		List<JSONObject> convertedNewsList = this.convert(newsList);
+		List<JSONObject> convertedNewsDetailList = this.convert(newsDetailList);
+		for(JSONObject news : convertedNewsList) {
+			String newsId = this.getValue("id", news);
+			for(JSONObject newsDetail : convertedNewsDetailList) {
+				String newsDetailId = this.getValue("id", newsDetail);
+				if(newsId.equals(newsDetailId)) {
+					String contentDetail = this.getValue("content", newsDetail);
+					news.put("contentDetail", contentDetail);
+				}
+			}
+		}
+		return convertedNewsList;
+	}
+	
+	public String getValue(String valueKey, JSONObject item) {
+		Object result = null;
+		Set<Entry<String, Object>> entrySet = item.entrySet();
+		for(Entry<String, Object> entry : entrySet) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			if(valueKey.equals(key)) {
+				result = value;
+				break;
+			}
+		}
+		return String.valueOf(result);
+	}
+	
+	public List<JSONObject> convert(List<String> list){
+		if(list == null) {
+			return new ArrayList<JSONObject>();
+		}
+		List<JSONObject> result = new ArrayList<JSONObject>();
+		for(String str : list) {
+			JSONObject jobj = JSONObject.parseObject(str);
+			result.add(jobj);
+		}
+		return result;
+	}
+	
+	
 
     public static void main(String[] args) {
     	BgoPageProcessor bp = new BgoPageProcessor();
         Spider.create(bp)
         .addUrl("https://api.biligame.com/news/list.action?gameExtensionId=45&positionId=2&pageNum=1&pageSize=1&typeId=")
         .run();
+        System.out.println("final result: " + bp.getResult());
     }
 }
--- src/main/java/com/haoli/fate/craw/BgoPipeline.java
+++ src/main/java/com/haoli/fate/craw/BgoPipeline.java
@@ -1,13 +1,15 @@
 package com.haoli.fate.craw;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.haoli.fate.dao.BgoDao;
+import com.haoli.fate.dao.NewsDao;
 
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -17,14 +19,49 @@ import us.codecraft.webmagic.pipeline.Pipeline;
 public class BgoPipeline implements Pipeline{
 	
 	@Autowired
-	BgoDao bgoDao;
+	NewsDao newsDao;
 
 	@Override
 	public void process(ResultItems resultItems, Task task) {
-		JSONArray array = JSONObject.parseArray(String.valueOf(resultItems.get("titleList")));
-		List<String> titleList = array.toJavaList(String.class);
-		
+		List<JSONObject> newsList = this.convertToList(resultItems, "newsList");
+		List<JSONObject> newsDetailList = this.convertToList(resultItems, "newsDetailList");
+		for(JSONObject news : newsList) {
+			String newsId = this.getValue("id", news);
+			for(JSONObject newsDetail : newsDetailList) {
+				String newsDetailId = this.getValue("id", newsDetail);
+				if(newsId.equals(newsDetailId)) {
+					String contentDetail = this.getValue("content", newsDetail);
+					news.put("contentDetail", contentDetail);
+				}
+			}
+		}
+		System.out.println(newsList);
 	}
 	
+	public String getValue(String valueKey, JSONObject item) {
+		Object result = null;
+		Set<Entry<String, Object>> entrySet = item.entrySet();
+		for(Entry<String, Object> entry : entrySet) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			if(valueKey.equals(key)) {
+				result = value;
+				break;
+			}
+		}
+		return String.valueOf(result);
+	}
 	
+	public List<JSONObject> convertToList(ResultItems resultItems, String key){
+		List<String> list = resultItems.get(key);
+		if(list == null) {
+			return new ArrayList<JSONObject>();
+		}
+		List<JSONObject> result = new ArrayList<JSONObject>();
+		for(String str : list) {
+			JSONObject jobj = JSONObject.parseObject(str);
+			result.add(jobj);
+		}
+		return result;
+	}
 }
