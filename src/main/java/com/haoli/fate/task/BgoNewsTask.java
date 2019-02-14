cc761e135d737a7588bde568caf894162534a7c6
lihao <lihao_100@boe.com.cn>
2019-02-14 13:22:58 +0800

    调整定时任务时间间隔

--- src/main/java/com/haoli/fate/task/BgoNewsTask.java
+++ src/main/java/com/haoli/fate/task/BgoNewsTask.java
@@ -16,10 +16,10 @@ public class BgoNewsTask {
 	DataService dataService;
 	
 	/**
-	 * 每天每隔四小时更新一次bgo新闻
+	 * 每天每隔8小时更新一次bgo新闻
 	 * @throws Exception 
 	 */
-	@Scheduled(cron = "0 0 0/4 * * ? ")
+	@Scheduled(cron = "0 0 0/6 * * ? ")
 	public void getBgoNews() throws Exception {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("pageSize", 5);
