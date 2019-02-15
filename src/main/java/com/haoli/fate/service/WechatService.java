67129066a3330ee9ab27fbabcbbb13e9f8cf0773
lihao <lihao_100@boe.com.cn>
2019-02-15 13:52:27 +0800

    fix

--- src/main/java/com/haoli/fate/service/WechatService.java
+++ src/main/java/com/haoli/fate/service/WechatService.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WechatService {
 	
 	
